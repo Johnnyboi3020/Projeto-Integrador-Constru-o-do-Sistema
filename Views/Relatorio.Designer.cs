1d786a1a0abca7c94dd9768c1874a377f24fd570
João Vitor <joaovitorzampier09@gmail.com>
2021-11-03 23:16:11 -0300

    nomeando botão e label

--- Views/Relatorio.Designer.cs
+++ Views/Relatorio.Designer.cs
@@ -39,9 +39,9 @@ namespace Projeto_Integrador_Construção_do_Sistema.Views
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(55, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Produtos";
             // 
             // button1
             // 
@@ -49,7 +49,7 @@ namespace Projeto_Integrador_Construção_do_Sistema.Views
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Executar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
@@ -64,7 +64,7 @@ namespace Projeto_Integrador_Construção_do_Sistema.Views
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(749, 380);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
